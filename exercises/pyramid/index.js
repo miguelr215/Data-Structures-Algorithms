bb0e389127d2427b3463603acedd46f48c9151c3
drexelramos <drexelramos@gmail.com>
2021-06-26 06:00:16 -0700

    steps & pyramid complete

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,50 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+
+// RECURSIVE SOLUTION
+function pyramid(n, row = 0, col = 0, level = '') {
+    const midpoint = Math.floor((2 * n - 1) / 2);
+    
+    // base case
+    if(n === row){
+        return;
+    }
+
+    // recursive case
+    if(level.length === (2 * n - 1)){
+        console.log(level);
+        pyramid(n, row + 1); 
+        return;
+    }
+    
+    if(midpoint - row <= level.length && midpoint + row >= level.length){
+        level += '#';
+    } else {
+        level += ' ';
+    }
+
+    pyramid(n, row, col + 1, level);
+}
 
 module.exports = pyramid;
+
+
+// ITERATIVE SOLUTION
+// function pyramid(n) {
+//     const midpoint = Math.floor((2 * n - 1) / 2);
+
+//     for(let row = 0; row < n; row++){
+//         let level = '';
+
+//         for(let col = 0; col < 2 * n - 1; col++){
+//             if(midpoint - row <= col && midpoint + row >= col){
+//                 level += '#';
+//             } else {
+//                 level += ' ';
+//             }
+//         }
+
+//         console.log(level);
+//     }
+// }
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,47 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// RECURSIVE SOLUTION
+function steps(n, row = 0, stair = '') {
+    
+    // base case
+    if(row === n){
+        return;
+    }
+
+    // recursive case
+    if(n === stair.length) {
+        console.log(stair);
+        steps(n, row + 1);
+        return;
+    }
+
+    if(stair.length <= row){
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+
+    steps(n, row, stair);
+    
+}
+
 
 module.exports = steps;
+
+// ITERATIVE SOLUTION
+// function steps(n) {
+
+//     for (let row = 0; row < n; row++) {
+//         let stair = '';
+
+//         for (let col = 0; col < n; col++){
+//             if(col <= row){
+//                 stair += '#';
+//             } else {
+//                 stair += ' ';
+//             }    
+//         }
+//         console.log(stair);
+//     }
+// }
